d00f61894063717ef78a797d139f2a0a39e949c6
JosefNagelschmidt <login@jn.contact>
2022-11-07 22:54:32 +0100

    Fix topic path

--- google-maps-directions-api/main.py
+++ google-maps-directions-api/main.py
@@ -23,7 +23,6 @@ def hello_pubsub(event, context):
 
     print("""This Function was triggered by messageId {} published at {} to {}
     """.format(context.event_id, context.timestamp, context.resource["name"]))
-
     if 'data' in event:
         name = base64.b64decode(event['data']).decode('utf-8')
     else:
