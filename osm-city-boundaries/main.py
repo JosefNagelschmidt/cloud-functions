248819cda8819a2c7c1f33fedbf12a6b7c82ec58
JosefNagelschmidt <login@jn.contact>
2022-11-07 22:14:52 +0100

    rename cloud function for city boundaries

--- osm-city-boundaries/main.py
+++ osm-city-boundaries/main.py
@@ -1,17 +1,3 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import json
 from datetime import date
 
@@ -21,7 +7,7 @@ from google.cloud import storage
 from osm2geojson import json2geojson
 
 
-def hello_gcs(event, context):
+def load_city_boundaries(event, context):
     """Background Cloud Function to be triggered by Cloud Storage.
        This generic function logs relevant data when a file is changed,
        and works for all Cloud Storage CRUD operations.
